42be16241485977ac11d01286cb021ebdb6535c7
gabrielgoc99 <gabrielgoc99@gmail.com>
2023-08-29 21:02:28 -0300

    Captura do tamanho da tela e geração randomica da imagem no body.

--- game.js
+++ game.js
@@ -3,11 +3,30 @@ var largura = 0
 
 function ajustaTamanhoTela() {
 
-    var altura = window.innerHeight
-    var largura = window.innerWidth
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
 
     console.log(altura, largura)
 
 }
 
 ajustaTamanhoTela()
+
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 100
+    var posicaoY = Math.floor(Math.random() * altura) - 100
+
+    console.log(posicaoX, posicaoY)
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
