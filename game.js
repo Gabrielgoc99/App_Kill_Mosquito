12ec2f8f74db9f7669469173e3ef5f425449c665
Gabrielgoc99 <gabrielgoc99@gmail.com>
2023-09-04 21:19:34 -0300

    Ajustes no painel html e no arquivo JS

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoTela() {
 
@@ -18,6 +19,15 @@ function posicaoRandomica() {
 
     if(document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if(vidas > 3) {
+
+        } else {
+
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+
+            vidas++
+        }
     }
 
     /*Aqui, definimos as posições dos eixos X e Y, definindo a variável
@@ -43,6 +53,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 
